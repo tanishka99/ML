b1ce3aa2cfc1994d8205ffcc7daa3ca218247efe
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-20 16:07:23 +0000

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,3 +1,10 @@
+create 2D numpy  based array with given conditions:
+
+i)   take input from user in terms of dimension like (3x2 or 6x7)
+ii)   fill this numpy array with random number
+iii)  store this array in a file
+
+
 import numpy as np
 
 x=int(input("Enter no of rows:"))
