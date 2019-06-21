e6a7cc77669152c31f00046506e4110f366bb0dc
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-21 14:37:31 +0000

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,3 +1,11 @@
+visualize data graphs 
+i)  take input from  a file where you have 4 rows and 5 columns
+ii)  columns having - student_name , marks , age , contact , study_hours
+iii)  visualize this data as pie chart
+iv)  file name must  student.csv with all column separated by ','
+
+
+
 #!usr/bin/python3
 import csv
 import pandas as pd
