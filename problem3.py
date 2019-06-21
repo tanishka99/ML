b9e72dd8e41369c93665f9b6313ee8ae4a4452d5
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-21 14:35:47 +0000

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=[['student_name','marks','age','contact','study_hours'],['tanu','89','19','8291047284','5'],['ritika','87','18','72910472628','6'],['shreya','90','19','8391047389','7']]
+
+with open('st.csv','w') as csvFile:
+	writer=csv.writer(csvFile)
+	writer.writerows(data)
+csvFile.close()
+
+df=pd.read_csv('/home/adhoc/ml_tasks/st.csv')
+print(df)
+study=df["study_hours"]
+name=df["student_name"]
+exp=[0.1,0,0.1]
+plt.pie(study,labels=name,explode=exp,autopct='%1.5f%%',shadow=True)
+plt.grid(color="red")
+plt.show()
